ef4e6c7d5101f1d8ee559e58667a55f2229093be
Davido159 <bibisiman@gmail.com>
2024-11-10 21:34:29 -0300

    Update db_utils.py

--- db_utils.py
+++ db_utils.py
@@ -1,11 +1,13 @@
-# db_utils.py
-from mysql.connector import connect
-from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME
+import pymysql.cursors
+from config import DB_HOST, DB_USER, DB_PASSWORD, DB_NAME  # Importando as variáveis de configuração
 
+# Função de conexão ao banco de dados
 def connect_db():
-    return connect(
+    connection = pymysql.connect(
         host=DB_HOST,
         user=DB_USER,
         password=DB_PASSWORD,
-        database=DB_NAME
+        database=DB_NAME,
+        cursorclass=pymysql.cursors.DictCursor  # Usando DictCursor para resultados como dicionário
     )
+    return connection
